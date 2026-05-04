cfaba40812313166f83784e137ffa2be61fcd836
2024125079 <167060056+2024125079@users.noreply.github.com>
2026-05-04 16:12:56 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 int main(void)
 {
   int N = 0;
-  int found = 0;
+  bool check = false;
   scanf("%d", &N);
-  
   int A = 900, B = 750, C = 200;
-  
-  for(int i = 1; i <= N/A; i++){
-    for(int j = 1; j <= N/B; j++){
+
+  for(int i = 1; i <= N/A; i++){    
+    for(int j = 2; j <= N/B; j+=2){
       for(int k = 1; k <= N/C; k++){
-        if((i*A + j*B + k*C) == N){
-            if(j % 2 == 0){
-                if(i > k || j > k){
-                  printf("%d %d %d\n", i, j, k);
-                  found = 1;
-                }
-            }
+        if((i*A + j*B + k*C) == N && (i > k || j > k)){
+            printf("%d %d %d\n", i, j, k);
+            check = true;
         }
       }
     }
   }
-  if(found == 0){
+  if(check == false){
     printf("none");
   }
   
